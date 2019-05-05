1a57818bbda9e1265c81b8d0afd4b6187327add9
Anders Nylund <anders.nylund.an@gmail.com>
2019-05-05 21:16:54 +0300

    Display basic product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 
 import ProductList from './components/ProductList';
 
 const App = () => (
   <Container>
+    <Header size="huge">
+      <span role="img" aria-label="drunk">
+        🥴
+      </span>
+      &nbsp; Billifyllo
+    </Header>
     <ProductList />
   </Container>
 );
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shape, string } from 'prop-types';
+import { shape, string, number } from 'prop-types';
 import { Card } from 'semantic-ui-react';
 
 const Product = ({ product }) => {
@@ -7,13 +7,19 @@ const Product = ({ product }) => {
     <Card>
       <Card.Content>
         <Card.Header>{product.nimi}</Card.Header>
+        <Card.Meta>{product.valmistaja}</Card.Meta>
         <Card.Description>{product.luonnehdinta}</Card.Description>
       </Card.Content>
       <Card.Content extra>
+        <Card.Meta>{`Tyyppi: ${product.tyyppi}`}</Card.Meta>
         <Card.Meta>{`Hinta: ${product.hinta} €`}</Card.Meta>
+        <Card.Meta>
+          {`Alkoholiprosentti: ${product.alkoholiprosentti} %`}
+        </Card.Meta>
         <Card.Meta>
           {`Alkoholin litrahinta: ${product.alkoholilitrahinta} €`}
         </Card.Meta>
+        <Card.Meta>{`Pakkaustyyppi: ${product.pakkaustyyppi}`}</Card.Meta>
       </Card.Content>
     </Card>
   );
@@ -22,6 +28,13 @@ const Product = ({ product }) => {
 Product.propTypes = {
   product: shape({
     nimi: string.isRequired,
+    luonnehdinta: string.isRequired,
+    hinta: number.isRequired,
+    alkoholilitrahinta: number.isRequired,
+    pakkaustyyppi: string.isRequired,
+    tyyppi: string.isRequired,
+    valmistaja: string.isRequired,
+    alkoholiprosentti: number.isRequired,
   }).isRequired,
 };
 
