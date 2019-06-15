1667f9646371919fae0125e94165a5fe6294b9b3
Anders Nylund <anders.nylund.an@gmail.com>
2019-06-15 15:41:16 +0300

    Implement debouncing of the search term

--- frontend/src/components/SearchField.jsx
+++ frontend/src/components/SearchField.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Search } from 'semantic-ui-react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { setSearchAction } from '../actions/filterActions';
 
+// Hook from https://usehooks.com/useDebounce/
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
 const SearchField = () => {
-  const search = useSelector(state => state.filter.search);
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSearchAction(debouncedSearchTerm));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchTerm]);
 
   return (
     <Search
       showNoResults={false}
-      value={search}
-      onSearchChange={e => dispatch(setSearchAction(e.target.value))}
+      value={searchTerm}
+      onSearchChange={e => setSearchTerm(e.target.value)}
     />
   );
 };
