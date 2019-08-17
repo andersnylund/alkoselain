8816ca1dadd84284c8f24f492aeb32dd40c24903
Anders Nylund <anders.nylund.an@gmail.com>
2019-08-17 13:15:10 +0300

    Change header font

--- frontend/src/components/common/Title.jsx
+++ frontend/src/components/common/Title.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from '@reach/router';
 
-import 'typeface-varela-round';
+import 'typeface-roboto-slab';
 
 const Container = styled.div`
   display: flex;
   padding: 2rem;
   justify-content: center;
   h1 {
-    font-family: 'Varela Round', Tahoma, Geneva, Verdana, sans-serif;
+    font-family: 'Roboto Slab', Tahoma, Geneva, Verdana, sans-serif;
     font-weight: 800;
     font-size: var(--size-8);
-    color: var(--grey-3);
-    text-shadow: 1px 1px 3px hsl(0, 100%, 10%);
+    color: var(--grey-2);
+    text-shadow: 1px 1px 2px hsl(0, 50%, 10%);
 
     @media (max-width: 600px) {
       font-size: var(--size-7);
